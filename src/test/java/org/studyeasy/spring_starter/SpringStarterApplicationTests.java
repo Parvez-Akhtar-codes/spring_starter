2c815d418deae47e44af29a681b5a6a18ae3ddd6
Parvez-Akhtar-codes <parvezakhtar310@gmail.com>
2024-05-25 18:41:33 +0530

    first commit

---
+++ src/test/java/org/studyeasy/spring_starter/SpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.studyeasy.spring_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
