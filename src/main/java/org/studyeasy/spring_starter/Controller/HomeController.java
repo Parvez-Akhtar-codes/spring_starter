7046ecb0fa33b0180baddd20ad7b1f371b2edfac
Parvez-Akhtar-codes <parvezakhtar310@gmail.com>
2024-05-26 11:57:06 +0530

    Spring Boot

--- src/main/java/org/studyeasy/spring_starter/Controller/HomeController.java
+++ src/main/java/org/studyeasy/spring_starter/Controller/HomeController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home(Model model){
         return "home";
     }
+    
+   
     
 }
