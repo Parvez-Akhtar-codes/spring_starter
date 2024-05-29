da778100b7bad116e13f97c029fbf01cc86a97d8
Parvez-Akhtar-codes <parvezakhtar310@gmail.com>
2024-05-29 16:19:26 +0530

    Spring Boot

--- src/main/java/org/studyeasy/spring_starter/config/WebSecurityConfig.java
+++ src/main/java/org/studyeasy/spring_starter/config/WebSecurityConfig.java
@@ -1,10 +1,11 @@
 package org.studyeasy.spring_starter.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -25,13 +26,33 @@ public class WebSecurityConfig {
     };
 
     @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
         .authorizeRequests()
         .antMatchers(WHITELIST)
         .permitAll()
         .anyRequest()
-        .authenticated();
+        .authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .loginProcessingUrl("/login")
+        .usernameParameter("email")
+        .passwordParameter("password")
+        .defaultSuccessUrl("/", true)
+        .failureUrl("/login?error")
+        .permitAll()
+        .and()
+        .logout()
+        .logoutUrl("/logout")
+        .logoutSuccessUrl("/logout?success")
+        .and()
+        .httpBasic();
 
         //TODO: remove these after upgrading the DB from H2 infile DB
         httpSecurity.csrf().disable();
--- src/main/java/org/studyeasy/spring_starter/services/AccountService.java
+++ src/main/java/org/studyeasy/spring_starter/services/AccountService.java
@@ -1,6 +1,7 @@
 package org.studyeasy.spring_starter.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.studyeasy.spring_starter.Models.Account;
 import org.studyeasy.spring_starter.repositories.AccountRepository;
@@ -12,7 +13,11 @@ public class AccountService {
     @Autowired
      private AccountRepository accountRepository;
 
+     @Autowired
+     private PasswordEncoder passwordEncoder;
+
      public Account save(Account account){
+      account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountRepository.save(account);
      }
 }
